c90150a4eca45846515b10f75665094c9533888d
Muhamad Irham Maulana <irhammaulana1077@gmail.com>
2021-10-23 10:21:06 +0700

    Update App.js

--- kuliner-padang/src/App.js
+++ kuliner-padang/src/App.js
@@ -1,19 +1,13 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import { NavigationBar } from './components';
-import { Home, Sukses } from './pages';
 
-const navigations = [
-  { name: 'Home', href: '#home' },
-  { name: 'Menu', href: '#menu'},
-];
+import { Home, Sukses } from './pages';
 
 export default class App extends Component {
   
   render() {
     return (
       <BrowserRouter>
-          <NavigationBar title1 = "kuliner" title2 = "Padang" navigations = {navigations} />
           <main>
             <Switch>
               <Route  path="/" component={Home} exact/>
